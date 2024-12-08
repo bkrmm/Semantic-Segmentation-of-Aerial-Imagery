8f3c1fd7557fa8abfaa58988679d727751ea2c2a
Bikramjeet Singh Bedi <96697911+bkrmm@users.noreply.github.com>
2024-12-09 04:04:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,273 @@
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from patchify import patchify
+from PIL import Image
+import segmentation_models_pytorch as smp
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from tensorflow import keras
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, concatenate # type: ignore
+from tensorflow.keras import Model # type: ignore
+
+root_directory = r'C:\DHANDA\Aerial-Semantic-Segmentation\Semantic segmentation dataset'
+
+patch_size = 256
+#____________________________________________________________________________
+
+image_dataset = []
+for path, subdirs, files in os.walk(root_directory):
+    #print(path)  
+    dirname = path.split(os.path.sep)[-1]
+    if dirname == 'images':   #Find all 'images' directories 
+        images = os.listdir(path)  #List of all image names in this subdirectory
+        for i, image_name in enumerate(images):  
+            if image_name.endswith(".jpg"):   #Only read jpg images...
+               
+                image = cv2.imread(path+"/"+image_name, 1)  #Read each image as BGR
+                SIZE_X = (image.shape[1]//patch_size)*patch_size #Nearest size divisible by our patch size
+                SIZE_Y = (image.shape[0]//patch_size)*patch_size #Nearest size divisible by our patch size
+                image = Image.fromarray(image)
+                image = image.crop((0 ,0, SIZE_X, SIZE_Y))  #Crop from top left corner
+                #image = image.resize((SIZE_X, SIZE_Y))  #Try not to resize for semantic segmentation
+                image = np.array(image)             
+       
+                #Extract patches from each image
+                print("Now patchifying image:", path+"/"+image_name)
+                patches_img = patchify(image, (patch_size, patch_size, 3), step=patch_size)  #Step=256 for 256 patches means no overlap    
+                for i in range(patches_img.shape[0]):
+                    for j in range(patches_img.shape[1]):
+                        
+                        single_patch_img = patches_img[i,j,:,:]
+                        
+                        #Use minmaxscaler instead of just dividing by 255. 
+                        scaler = MinMaxScaler()
+                        single_patch_img = scaler.fit_transform(single_patch_img.reshape(-1, single_patch_img.shape[-1])).reshape(single_patch_img.shape)
+                        
+                        #single_patch_img = (single_patch_img.astype('float32')) / 255. 
+                        single_patch_img = single_patch_img[0] #Drop the extra unecessary dimension that patchify adds.                               
+                        image_dataset.append(single_patch_img)
+
+#____________________________________________________________________________
+
+mask_dataset = []
+for path, subdirs, files in os.walk(root_directory):
+    dirname = path.split(os.path.sep)[-1]
+    if dirname == 'masks':
+        masks = os.listdir(path)
+        for i, mask_name in enumerate(masks):
+            if mask_name.endswith(".png"):
+
+                mask = cv2.imread(path+"/"+mask_name, 1)
+                mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+                SIZE_X = (mask.shape[1]//patch_size)*patch_size
+                SIZE_Y = (mask.shape[0]//patch_size)*patch_size
+                mask = Image.fromarray(mask)
+                mask = mask.crop((0, 0, SIZE_X, SIZE_Y))
+                mask = np.array(mask)
+
+                print("Now patchifying mask:", path+"/"+mask_name)
+                patches_mask = patchify(mask, (patch_size, patch_size, 3), step=patch_size)
+
+                for i in range(patches_mask.shape[0]):
+                    for j in range(patches_mask.shape[1]):
+
+                        single_patch_mask = patches_mask[i,j,:,:]
+                        single_patch_mask = single_patch_mask[0]
+                        mask_dataset.append(single_patch_mask)
+
+image_dataset = np.array(image_dataset)
+mask_dataset = np.array(mask_dataset)
+print(image_dataset.shape)
+print(mask_dataset.shape)
+#____________________________________________________________________________
+"""
+import random
+import numpy as np
+image_number = random.randint(0, len(image_dataset))
+plt.figure(figsize=(12, 6))
+plt.subplot(121)
+plt.imshow(np.reshape(image_dataset[image_number], (patch_size, patch_size, 3)))
+plt.subplot(122)
+plt.imshow(np.reshape(mask_dataset[image_number], (patch_size, patch_size, 3)))
+plt.show()
+"""
+Building = '#3C1098'.lstrip('#')
+Building = np.array(tuple(int(Building[i:i+2], 16) for i in (0, 2, 4))) # 60, 16, 152
+
+Land = '#8429F6'.lstrip('#')
+Land = np.array(tuple(int(Land[i:i+2], 16) for i in (0, 2, 4))) #132, 41, 246
+
+Road = '#6EC1E4'.lstrip('#') 
+Road = np.array(tuple(int(Road[i:i+2], 16) for i in (0, 2, 4))) #110, 193, 228
+
+Vegetation =  'FEDD3A'.lstrip('#') 
+Vegetation = np.array(tuple(int(Vegetation[i:i+2], 16) for i in (0, 2, 4))) #254, 221, 58
+
+Water = 'E2A929'.lstrip('#') 
+Water = np.array(tuple(int(Water[i:i+2], 16) for i in (0, 2, 4))) #226, 169, 41
+
+Unlabeled = '#9B9B9B'.lstrip('#') 
+Unlabeled = np.array(tuple(int(Unlabeled[i:i+2], 16) for i in (0, 2, 4))) #155, 155, 155
+
+label = single_patch_mask
+#____________________________________________________________________________
+
+def rgb_to_2D_label(label):
+    """
+    Suply our labale masks as input in RGB format. 
+    Replace pixels with specific RGB values ...
+    """
+    label_seg = np.zeros(label.shape,dtype=np.uint8)
+    label_seg [np.all(label == Building,axis=-1)] = 0
+    label_seg [np.all(label==Land,axis=-1)] = 1
+    label_seg [np.all(label==Road,axis=-1)] = 2
+    label_seg [np.all(label==Vegetation,axis=-1)] = 3
+    label_seg [np.all(label==Water,axis=-1)] = 4
+    label_seg [np.all(label==Unlabeled,axis=-1)] = 5
+    
+    label_seg = label_seg[:,:,0]  #Just take the first channel, no need for all 3 channels
+    
+    return label_seg
+
+labels = []
+for i in range(mask_dataset.shape[0]):
+    label = rgb_to_2D_label(mask_dataset[i])
+    labels.append(label)    
+
+labels = np.array(labels)   
+labels = np.expand_dims(labels, axis=3)
+ 
+
+print("Unique labels in label dataset are: ", np.unique(labels))
+#____________________________________________________________________________
+
+n_classes = len(np.unique(labels))
+import tensorflow.keras as keras # type: ignore
+labels_cat = keras.utils.to_categorical(labels, num_classes=n_classes)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(image_dataset, labels_cat, test_size = 0.20, random_state = 42)
+
+
+#######################################
+#Parameters for model
+# Segmentation models losses can be combined together by '+' and scaled by integer or float factor
+# set class weights for dice_loss
+# from sklearn.utils.class_weight import compute_class_weight
+
+# weights = compute_class_weight('balanced', np.unique(np.ravel(labels,order='C')), 
+#                               np.ravel(labels,order='C'))
+# print(weights)
+
+weights = [0.1666, 0.1666, 0.1666, 0.1666, 0.1666, 0.1666]
+
+def dice_loss(y_true, y_pred):
+    smooth = 1e-6
+    y_true_f = tf.keras.backend.flatten(y_true)
+    y_pred_f = tf.keras.backend.flatten(y_pred)
+    intersection = tf.keras.backend.sum(y_true_f * y_pred_f)
+    return 1 - (2. * intersection + smooth) / (tf.keras.backend.sum(y_true_f) + tf.keras.backend.sum(y_pred_f) + smooth)
+
+def focal_loss(y_true, y_pred):
+    gamma = 2.0
+    alpha = 0.25
+    epsilon = 1e-6
+    
+    y_pred = tf.clip_by_value(y_pred, epsilon, 1. - epsilon)
+    focal_loss = -alpha * tf.pow(1. - y_pred, gamma) * y_true * tf.math.log(y_pred)
+    return tf.reduce_mean(focal_loss)
+
+def total_loss(y_true, y_pred):
+    return dice_loss(y_true, y_pred) + focal_loss(y_true, y_pred)
+
+IMG_HEIGHT = X_train.shape[1]
+IMG_WIDTH  = X_train.shape[2]
+IMG_CHANNELS = X_train.shape[3]
+
+# Add this function before using it in metrics
+def jacard_coef(y_true, y_pred):
+    y_true_f = tf.keras.backend.flatten(y_true)
+    y_pred_f = tf.keras.backend.flatten(y_pred)
+    intersection = tf.keras.backend.sum(y_true_f * y_pred_f)
+    return (intersection + 1.0) / (tf.keras.backend.sum(y_true_f) + tf.keras.backend.sum(y_pred_f) - intersection + 1.0)
+
+metrics=['accuracy', jacard_coef]
+
+def multi_unet_model(n_classes, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS):
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    
+    #Contraction path
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+    
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+     
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+     
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+     
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    
+    #Expansive path 
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+     
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+     
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+     
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+     
+    outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
+     
+    model = Model(inputs=[inputs], outputs=[outputs])
+    
+    return model
+
+def get_model():
+    return multi_unet_model(n_classes=n_classes, IMG_HEIGHT=IMG_HEIGHT, IMG_WIDTH=IMG_WIDTH, IMG_CHANNELS=IMG_CHANNELS)
+
+model = get_model()
+model.compile(optimizer='adam', 
+             loss=total_loss, 
+             metrics=metrics,
+             weighted_metrics=['accuracy', jacard_coef])
+#model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=metrics)
+model.summary()
+
+
+class_weights = dict(zip(range(n_classes), weights))
+history1 = model.fit(X_train, y_train, 
+                    batch_size = 16, 
+                    verbose=1, 
+                    epochs=10#0, 
+                    validation_data=(X_test, y_test), 
+                    shuffle=False)
+try:
+    from simple_multi_unet_model import multi_unet_model, jacard_coef
+except ImportError:
+    print("Error: Could not import simple_multi_unet_model")
+    print("Current directory:", os.getcwd())
+    print("Available files:", os.listdir())
+
